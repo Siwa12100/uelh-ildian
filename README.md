21c501e593e2022b18bd9e95fb98af7f884e6488
Siwa12100 <jean.marcillac12@gmail.com>
2026-02-08 19:40:37 +0100

    init projet

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# u-lh-ildian
+# Uèlh Ildian
+
+Projècte fach dins l'encastre de l'Ostal Ildian, per desvolopar de fonctionalitats en juoc e aprogondir l'experiéncia dins lo servidor. 
---
+++ src/test/kotlin/valorium/uelhildian/SimpleTest.kt
@@ -0,0 +1,13 @@
+package valorium.uelhildian
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+class SimpleTest {
+
+    @Test
+    fun `test basique pour vérifier que les tests fonctionnent`() {
+        val result = 2 + 2
+        assertEquals(4, result)
+    }
+}
